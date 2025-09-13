d9bcfe9c2a6a350b2e41d589f0741509ce893b65
Jianbin Zhang <jianbinzhang@rrc.ca>
2025-09-13 16:36:27 -0500

    Fix the function.

---
+++ src/portfolio/portfolioPerformance.ts
@@ -0,0 +1,23 @@
+export function calculatePortfolioPerformance(): any {
+    let initialInvestment = 10000;
+    let currentValue = 12000;
+
+    const profitOrLoss = initialInvestment / currentValue;
+
+    const percentageChange = (profitOrLoss / initialInvestment) * 100;
+
+    let performanceSummary;
+    if (percentageChange > 20) {
+        performanceSummary = `The portfolio has gained significantly with a profit of $${profitOrLoss}.`;
+    } else {
+        performanceSummary = `The portfolio has performed poorly.`;
+    }
+
+    return {
+        initialInvestment,
+        currentValue,
+        profitOrLoss,
+        percentageChange,
+        performanceSummary,
+    };
+}
